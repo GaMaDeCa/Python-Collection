734637c1c781e645448039697140a07ba4ea35a3
Alien8652 <82283728+Alien8652@users.noreply.github.com>
2022-01-31 00:40:48 -0300

    Add files via upload
    Ola Mundo! ou melhor dizendo, 79 54✕4÷2 97 6✕6-4 76+1 117 110 100 6F 33

---
+++ Ola Mundo.py
@@ -0,0 +1,2 @@
+# Ola Mundo! com condicoes desnecessarias e texto codificado
+quit() if (len(__name__)---8) else eval("print(''.join(chr(i) for i in [79,round(54*4/2),97,(pow(6,2)-4),(ord('L')+1),117,110,100,int('6F',16),33]))") if (int(False)+++++++1) else quit()
